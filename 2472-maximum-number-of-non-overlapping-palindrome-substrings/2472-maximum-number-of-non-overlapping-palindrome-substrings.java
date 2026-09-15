635b890e2ba729166342fa7aed7493493b61e10f
Nikhil Kumar <patelnikhil5245@gmail.com>
2026-09-15 23:48:44 +0530

    Time: 1 ms (100%), Space: 42.8 MB (89.95%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -1,43 +1,33 @@
 class Solution {
     public int maxPalindromes(String s, int k) {
         int n = s.length();
+        int ans = 0,
+            start = 0;
 
-        boolean[][] pal = new boolean[n][n];
+        for (int r = k - 1; r < n; ++r) {
+            int l = r - k + 1;
+            if (l >= start && check(s, l, r)) {
+                ++ans;
+                start = r + 1;
+                continue;
+            }
 
-        // Find all palindromic substrings
-        for (int i = n - 1; i >= 0; i--) {
-
-            for (int j = i; j < n; j++) {
-
-                if (s.charAt(i) == s.charAt(j) &&
-                    (j - i <= 2 || pal[i + 1][j - 1])) {
-
-                    pal[i][j] = true;
-                }
+            l = r - k;
+            if (l >= start && check(s, l, r)) {
+                ++ans;
+                start = r + 1;
             }
         }
 
-        // dp[i] = maximum number of substrings
-        // using first i characters
-        int[] dp = new int[n + 1];
+        return ans;
+    }
 
-        for (int i = 1; i <= n; i++) {
-
-            // Don't take a substring ending at i-1
-            dp[i] = dp[i - 1];
-
-            // Try every substring ending at i-1
-            for (int j = 0; j < i; j++) {
-
-                int length = i - j;
-
-                if (length >= k && pal[j][i - 1]) {
-
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
+    private boolean check(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l++) != s.charAt(r--)) {
+                return false;
             }
         }
-
-        return dp[n];
+        return true;
     }
 }
