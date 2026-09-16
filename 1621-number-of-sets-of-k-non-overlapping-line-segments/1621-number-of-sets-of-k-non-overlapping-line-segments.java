439f6d1849c6d2eaccba3c9d783639e8bd6c1d28
Nikhil Kumar <patelnikhil5245@gmail.com>
2026-09-16 21:03:54 +0530

    Time: 31 ms (63.23%), Space: 143.9 MB (38.71%) - LeetHub

---
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
@@ -0,0 +1,17 @@
+class Solution {
+    static final long MOD = 1000000007;
+    public int numberOfSets(int n, int k) {
+        long[][] dp = new long[n + k][2 * k + 1];
+
+        // dp[i][j] = C(i, j)
+        for (int i = 0; i <= n + k - 1; i++) {
+            dp[i][0] = 1;
+
+            for (int j = 1; j <= 2 * k && j <= i; j++) {
+                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % MOD;
+            }
+        }
+
+        return (int) dp[n + k - 1][2 * k]; 
+    }
+}
